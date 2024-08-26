8e105f9564d25c6237b05442bf0ef6eed067f66e
Nikita Maximov <john.maks595@gmail.com>
2024-08-26 19:08:20 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,14 @@ import packageJson from "./package.json";
 function jsccPlugin() {
 	return {
 		name: "vite-plugin-jscc",
-		transform(code, id) {
+		transform(code: string[], id: string) {
 			if (id.endsWith(".jscc")) {
-				// Определяем имя переменной на основе имени файла
 				const functionName = id.includes("DiabloSpawn")
 					? "DiabloSpawn"
 					: id.includes("MpqCmp")
 					? "MpqCmp"
 					: "Diablo";
 
-				// Ожидаем, что содержимое файла уже содержит IIFE, поэтому просто оборачиваем и экспортируем результат
 				return {
 					code: `${code}\nexport default ${functionName};`,
 					map: null,
