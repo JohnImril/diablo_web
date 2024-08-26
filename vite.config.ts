3104b1dc1dabcbe5441b527d8819d39d9b21161d
Nikita Maximov <john.maks595@gmail.com>
2024-08-27 01:41:26 +0300

    add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,35 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import wasm from "vite-plugin-wasm";
 import packageJson from "./package.json";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
 	base: "/diablo_web/",
-	plugins: [react(), wasm()],
+	plugins: [
+		react(),
+		wasm(),
+		VitePWA({
+			registerType: "autoUpdate",
+			workbox: {
+				runtimeCaching: [
+					{
+						urlPattern: /\/.*\.(?:html|css|js|wasm)$/,
+						handler: "CacheFirst",
+						options: {
+							cacheName: "static-resources",
+							expiration: {
+								maxEntries: 50,
+								maxAgeSeconds: 30 * 24 * 60 * 60,
+							},
+						},
+					},
+				],
+				navigateFallback: "/index.html",
+				navigateFallbackAllowlist: [/^(?!\/__).*/],
+				maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+			},
+		}),
+	],
 	define: {
 		"import.meta.env.VERSION": JSON.stringify(packageJson.version),
 	},
