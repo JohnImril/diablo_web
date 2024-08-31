3dd8d9f4d11b2daa623c1a5194dfb1d343e232c4
Nikita Maximov <john.maks595@gmail.com>
2024-08-31 23:56:52 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/diablo_web/sw.js', { scope: '/diablo_web/' })})}
