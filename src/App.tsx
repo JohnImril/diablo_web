3d1682525a697a109fa199d93fe783155d5d570b
Nikita Maximov <john.maks595@gmail.com>
2024-09-11 00:36:17 +0300

    fix button style

--- src/App.tsx
+++ src/App.tsx
@@ -760,33 +760,37 @@ class App extends Component<object, IState> {
 					<ul className="saveList">
 						{Object.entries(save_names).map(([name, info]) => (
 							<li key={name}>
-								<div>{name}</div>
-								{info ? (
-									<div className="info">
-										{info.name} (lv. {info.level} {plrClass[info.cls]})
-									</div>
-								) : null}
-								<div className="btnDownload" onClick={() => this.downloadSave(name)}>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 24 24"
-										fill="currentColor"
-										width="16px"
-										height="16px"
-									>
-										<path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z" />
-									</svg>
+								<div>
+									<div>{name}</div>
+									{info ? (
+										<div className="info">
+											{info.name} (lv. {info.level} {plrClass[info.cls]})
+										</div>
+									) : null}
 								</div>
-								<div className="btnRemove" onClick={() => this.removeSave(name)}>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 24 24"
-										fill="currentColor"
-										width="16px"
-										height="16px"
-									>
-										<path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
-									</svg>
+								<div className="btn">
+									<div className="btnDownload" onClick={() => this.downloadSave(name)}>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 24 24"
+											fill="currentColor"
+											width="16px"
+											height="16px"
+										>
+											<path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z" />
+										</svg>
+									</div>
+									<div className="btnRemove" onClick={() => this.removeSave(name)}>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 24 24"
+											fill="currentColor"
+											width="16px"
+											height="16px"
+										>
+											<path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+										</svg>
+									</div>
 								</div>
 							</li>
 						))}
