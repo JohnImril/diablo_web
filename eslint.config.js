5092ad97c312d69505a371528f6915e5192fdcf6
Nikita Maximov <john.maks595@gmail.com>
2024-11-12 22:40:03 +0300

    Update pack

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,7 @@ export default tseslint.config(
 		},
 		rules: {
 			...reactHooks.configs.recommended.rules,
+			"@typescript-eslint/no-explicit-any": "warn",
 			"react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
 		},
 	}
